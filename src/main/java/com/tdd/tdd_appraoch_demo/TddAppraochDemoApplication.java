490cc784f1ec50aad9485a4e56b66b5c8b61c712
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-04 11:34:43 +0530

    TDD_Approach, initial commit

---
+++ src/main/java/com/tdd/tdd_appraoch_demo/TddAppraochDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.tdd_appraoch_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAppraochDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddAppraochDemoApplication.class, args);
+	}
+
+}
