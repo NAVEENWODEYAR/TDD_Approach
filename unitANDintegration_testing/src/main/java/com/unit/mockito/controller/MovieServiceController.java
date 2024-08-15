bd239706b66fffe0b342da7122938fcb6917e03b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-15 20:03:46 +0530

    swagger added for rest endpoints

--- unitANDintegration_testing/src/main/java/com/unit/mockito/controller/MovieServiceController.java
+++ unitANDintegration_testing/src/main/java/com/unit/mockito/controller/MovieServiceController.java
@@ -11,6 +11,7 @@ import com.unit.mockito.service.MovieService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
 /**
  * @author Naveen K Wodeyaar,
  * @Date 15-Aug-2024
@@ -31,8 +32,32 @@ public class MovieServiceController {
 	
 	@PostMapping("/add")
     @Operation(summary = "Add new movie", description = "Add a new movie to the system.")
-    public ResponseEntity<Object> addDepartment(@RequestBody Movie request) {
+    public ResponseEntity<Object> addMovie(@RequestBody Movie request) {
         return ResponseEntity.ok(movieService.addMovie(request));
+    }
+	
+	@GetMapping("/getById/{id}")
+    @Operation(summary = "Find by movie-Id", description = "Fetches the movie by the provided movie-id,")
+    public ResponseEntity<Object> getMovieById(@PathParam("id") Long id) {
+        return ResponseEntity.ok(movieService.getByMovieId(id));
+    }
+	
+	@GetMapping("/list")
+    @Operation(summary = "Find movies list", description = "Fetches the available movies list,")
+    public ResponseEntity<Object> getMovies(@PathParam("id") Long id) {
+        return ResponseEntity.ok(movieService.getByMovieId(id));
+    }
+	
+	@PutMapping("/update/{id}")
+    @Operation(summary = "Update movie", description = "Updates the movie by its id,")
+    public ResponseEntity<Object> updateMovie(@PathParam("id") Long id,@RequestBody Movie request) {
+        return ResponseEntity.ok(movieService.updateMovie(request, id));
+    }
+	
+	@DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete movie", description = "Removes the provided movie from the db,")
+    public ResponseEntity<Object> delteMovie(@PathParam("id") Long id) {
+        return ResponseEntity.ok(movieService.deleteMovie(id));
     }
 	
 }
