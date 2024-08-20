2e20a0474da29365b22eaaf227b4fd0410125b19
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-21 00:41:01 +0530

    swagger implemented

---
+++ tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.tdd.tdd_appraoch_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 21-Aug-2024
+ */
+@Configuration
+public class SecurityConfig {
+
+}
