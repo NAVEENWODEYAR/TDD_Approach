b4e2144436fbd7cf05b93d6c6a8f6d3e715e99ec
Naveen <naveenkrishnawodeyar@gmail.com>
2024-08-21 17:37:52 +0530

    Swagger added

--- tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/config/SecurityConfig.java
+++ tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/config/SecurityConfig.java
@@ -1,12 +1,41 @@
 package com.tdd.tdd_appraoch_demo.config;
 
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 /**
  * @author Naveen K Wodeyar
  * @date 21-Aug-2024
  */
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
 
+	 @Bean
+	    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeRequests()
+	                .anyRequest().authenticated()
+	                .and()
+	            .formLogin()
+	                .loginPage("/login")
+	                .permitAll();
+	        return http.build();
+	    }
+	 
+	 @Bean
+	    public UserDetailsService userDetailsService() {
+	        UserDetails user = User.withDefaultPasswordEncoder()
+	            .username("user")
+	            .password("password")
+	            .roles("USER")
+	            .build();
+	        return new InMemoryUserDetailsManager(user);
+	    }
 }
--- unitANDintegration_testing/src/main/java/com/unit/mockito/config/SwaggerConfig.java
+++ unitANDintegration_testing/src/main/java/com/unit/mockito/config/SwaggerConfig.java
@@ -1,50 +1,50 @@
-package com.unit.mockito.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-
-/**
- * @author Naveen K Wodeyaar,
- * @Date 18-Aug-2024
- */
-@Configuration
-public class SwaggerConfig {
-	
-//	@Bean
-//	public OpenAPI openAPI() {
-//		return new OpenAPI()
-//					.info(new Info()
-//							.title("Unit_Integration_Testing")
-//							.description("Spring Boot Unit Testing and Integration Testing with JUnit,\r\n"
-//									+ "		Mockito and MockMvc,")
-//							.version("2.0")
-//							.description("Testing demo,,")
-//							.contact(new Contact().name("Naveen K Wodeyar").url("naveen.co.in").email("naveenwodeyar@yahoo.com"))
-//							.license(new License().name("My License").url("www.license.co.in"))
-//							);
-//	}
+//package com.unit.mockito.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Contact;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.info.License;
+//
+///**
+// * @author Naveen K Wodeyaar,
+// * @Date 18-Aug-2024
+// */
+//@Configuration
+//public class SwaggerConfig {
 //	
-	 @Bean
-	    public OpenAPI openAPI() {
-	        return new OpenAPI()
-	                    .info(new Info()
-	                            .title("Unit_Integration_Testing")
-	                            .description("Spring Boot Unit Testing and Integration Testing with JUnit, Mockito, and MockMvc.")
-	                            .version("1.0")
-	                            .contact(new Contact()
-	                                    .name("Naveen K Wodeyar")
-	                                    .url("http://naveen.co.in")
-	                                    .email("naveenwodeyar@yahoo.com"))
-	                            .license(new License()
-	                                    .name("My License")
-	                                    .url("http://www.license.co.in"))
-	                            );
-	    }
-
-
-}
+////	@Bean
+////	public OpenAPI openAPI() {
+////		return new OpenAPI()
+////					.info(new Info()
+////							.title("Unit_Integration_Testing")
+////							.description("Spring Boot Unit Testing and Integration Testing with JUnit,\r\n"
+////									+ "		Mockito and MockMvc,")
+////							.version("2.0")
+////							.description("Testing demo,,")
+////							.contact(new Contact().name("Naveen K Wodeyar").url("naveen.co.in").email("naveenwodeyar@yahoo.com"))
+////							.license(new License().name("My License").url("www.license.co.in"))
+////							);
+////	}
+////	
+//	 @Bean
+//	    public OpenAPI openAPI() {
+//	        return new OpenAPI()
+//	                    .info(new Info()
+//	                            .title("Unit_Integration_Testing")
+//	                            .description("Spring Boot Unit Testing and Integration Testing with JUnit, Mockito, and MockMvc.")
+//	                            .version("1.0")
+//	                            .contact(new Contact()
+//	                                    .name("Naveen K Wodeyar")
+//	                                    .url("http://naveen.co.in")
+//	                                    .email("naveenwodeyar@yahoo.com"))
+//	                            .license(new License()
+//	                                    .name("My License")
+//	                                    .url("http://www.license.co.in"))
+//	                            );
+//	    }
+//
+//
+//}
