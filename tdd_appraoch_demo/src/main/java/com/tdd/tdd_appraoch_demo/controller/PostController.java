8a8c7200fa3b0655a48df30db4f065d495d2c15c
Naveen <naveenkrishnawodeyar@gmail.com>
2025-01-06 22:46:37 +0530

    lombok error

--- tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/controller/PostController.java
+++ tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/controller/PostController.java
@@ -2,7 +2,6 @@ package com.tdd.tdd_appraoch_demo.controller;
 
 
 import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.tdd.tdd_appraoch_demo.dto.Post;
 import com.tdd.tdd_appraoch_demo.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
