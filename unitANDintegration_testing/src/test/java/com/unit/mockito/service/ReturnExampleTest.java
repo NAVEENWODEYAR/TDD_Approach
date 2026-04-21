dd17028625f0953ee25c6e5d9528f7a8b41dd8f4
Naveen <naveenkrishnawodeyar@gmail.com>
2026-04-21 23:30:55 +0530

    organise import statements

--- unitANDintegration_testing/src/test/java/com/unit/mockito/service/ReturnExampleTest.java
+++ unitANDintegration_testing/src/test/java/com/unit/mockito/service/ReturnExampleTest.java
@@ -1,18 +1,11 @@
 package com.unit.mockito.service;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import org.junit.jupiter.api.Test;
 
 /**
  * @author Naveen K Wodeyaar,
