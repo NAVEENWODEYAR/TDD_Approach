ca9fa807c4b80706d49514a549d0f34e175d22af
Naveen <naveenkrishnawodeyar@gmail.com>
2026-05-22 01:00:17 +0530

    Java interview programs list

---
+++ tdd_appraoch_demo/src/main/java/com/tdd/tdd_appraoch_demo/service/impl/JavaProgramsListOnly.java
@@ -0,0 +1,68 @@
+/**
+ * @author Naveen K Wodeyar
+ * @date 22-May-2026
+ */
+package com.tdd.tdd_appraoch_demo.service.impl;
+
+//Most Asked Java Tricky Programs List
+
+public class JavaProgramsListOnly {
+
+ public static void main(String[] args) {
+
+     System.out.println("===== MOST ASKED JAVA TRICKY PROGRAMS =====");
+
+     System.out.println("1. String vs StringBuilder Comparison");
+     System.out.println("2. String Immutability Example");
+     System.out.println("3. == vs equals() Example");
+     System.out.println("4. Finally Block Execution");
+     System.out.println("5. Integer Cache Example");
+     System.out.println("6. Method Overloading Example");
+     System.out.println("7. Method Overriding Example");
+     System.out.println("8. Constructor Chaining");
+     System.out.println("9. Static Block Execution");
+     System.out.println("10. Singleton Class");
+     System.out.println("11. Immutable Class");
+     System.out.println("12. Abstract Class Example");
+     System.out.println("13. Interface Default Method");
+     System.out.println("14. Multiple Inheritance using Interface");
+     System.out.println("15. Exception Handling");
+     System.out.println("16. Custom Exception");
+     System.out.println("17. Try-Catch-Finally Flow");
+     System.out.println("18. Throw vs Throws");
+     System.out.println("19. Multithreading Example");
+     System.out.println("20. Runnable vs Thread");
+     System.out.println("21. Synchronization Example");
+     System.out.println("22. Deadlock Program");
+     System.out.println("23. Producer Consumer Problem");
+     System.out.println("24. Thread Communication");
+     System.out.println("25. Race Condition Example");
+     System.out.println("26. HashMap Internal Working");
+     System.out.println("27. ConcurrentHashMap Example");
+     System.out.println("28. HashSet Example");
+     System.out.println("29. Comparable vs Comparator");
+     System.out.println("30. Fail Fast vs Fail Safe");
+     System.out.println("31. Stream API Example");
+     System.out.println("32. Lambda Expression");
+     System.out.println("33. Functional Interface");
+     System.out.println("34. Optional Class Example");
+     System.out.println("35. Java 8 Date and Time API");
+     System.out.println("36. File Handling Example");
+     System.out.println("37. Serialization and Deserialization");
+     System.out.println("38. Clone Method Example");
+     System.out.println("39. Reflection API");
+     System.out.println("40. Memory Leak Example");
+     System.out.println("41. Garbage Collection Example");
+     System.out.println("42. JVM Memory Areas");
+     System.out.println("43. ClassLoader Example");
+     System.out.println("44. Volatile Keyword");
+     System.out.println("45. Double Checked Locking");
+     System.out.println("46. ExecutorService Example");
+     System.out.println("47. Callable and Future");
+     System.out.println("48. ForkJoinPool Example");
+     System.out.println("49. Design Pattern Programs");
+     System.out.println("50. Microservices Basic Java Program");
+
+     System.out.println("===========================================");
+ }
+}
